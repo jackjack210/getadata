7d934f544da3bfdff7c9a51e1ee8afcb5db12901
UDDESHJAIS21 <uddeshjais.21@gmail.com>
2021-12-22 13:22:19 +0530

    'asdf'

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 dotenv.config({path:"./config.env"});
 
@@ -15,6 +19,10 @@ app.use(express.json());
 
 const feedback = require("./getdata");
 app.use("/feed", feedback);
+
+app.get("/", (req, res) => {
+    res.send("API link for backend");
+  });
 
 app.listen(3005, () =>{
     console.log("http://localhost:3005");
